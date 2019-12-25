4f40f9e0bf3670d651d559f3cbb6293ed97bf213
daniyar <daniyarsansyzbayev@protonmail.com>
2019-12-25 08:27:59 +0600

    minor layout changes

--- app/src/main/java/com/example/moviedbappwithmap/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/moviedbappwithmap/MyFirebaseMessagingService.kt
@@ -9,6 +9,7 @@ import android.media.RingtoneManager
 import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import com.example.moviedbappwithmap.presentation.MainActivity
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
@@ -40,12 +41,20 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         val pendingIntent = PendingIntent.getActivity(this,0, intent, PendingIntent.FLAG_ONE_SHOT)
         val channelId = getString(R.string.default_notification_channel_id)
         val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val message = "\"Super Вторник\" – супер-выгодное предложение для всех!\n" +
+                "Каждый вторник покупайте билеты в кино по выгодным ценам:\n" +
+                "Обычные залы: взрослый - 1000 тг, студенческий - 800 тг, детский - 600 тг.\n" +
+                "В зале IMAX: взрослый - 1500 тг, детский - 1000 тг, студенческий - 1300 тг,\n" +
+                "В залах Premium: First Class Laser - 7500 тг, Comfort - 1300 тг."
         val notificationBuilder = NotificationCompat.Builder(this, channelId)
-            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setSmallIcon(R.drawable.ic_movie_filter_black_24dp)
             .setContentTitle("Movie DB App")
+            .setStyle(NotificationCompat.BigTextStyle()
+                .bigText(message))
             .setContentText(messageBody)
             .setAutoCancel(true)
             .setSound(defaultSoundUri)
+            .setColor(ContextCompat.getColor(this,R.color.colorAccent))
             .setContentIntent(pendingIntent)
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
